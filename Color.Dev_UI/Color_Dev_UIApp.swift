32002f13bb43b0c2a0008e64fd5be39676ed8d07
Shubham Singh <singh.shubham0812@gmail.com>
2022-10-18 15:30:22 +0530

    ✨ Initialise Project

---
+++ Color.Dev_UI/Color_Dev_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Color_Dev_UIApp.swift
+//  Color.Dev_UI
+//
+//  Created by Shubham Singh on 18/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Color_Dev_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
